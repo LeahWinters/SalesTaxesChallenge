358fbee4fd495658c1cb6fd499ac26de8b48eea6
Leah Winters <leahwinters@Leahs-MBP.home>
2023-07-05 13:16:38 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,10 @@ import React, { StrictMode } from 'react';
 import { createRoot }  from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App/App.tsx';
-// import './index.css';
-// import { createRoot } from ''
+
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container as Element | DocumentFragment);
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
 
 root.render(
   <StrictMode>
