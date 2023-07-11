3198bb445248d7667eb608e69cd9e1f80de57341
Leah Winters <leahwinters@Leahs-MBP.home>
2023-07-11 18:16:55 -0400

    formCreation - Updated Form component with styling and small functionality changes to store user inputs

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,35 +1,71 @@
-import React, { useState, FC } from 'react';
+import React, { useState, useEffect } from 'react';
+import AddedGoodsDataInterface from '../interfacesData';
 import './Form.css';
 
-interface Form {
-
+interface FormInterface {
+  addGoodsToGoodsData: () => void;
 };
 
-const Form: FC = () => {
+const Form = ({ addGoodsToGoodsData }: FormInterface) => {
   const [goodsName, setGoodsName] = useState('');
   const [category, setCategory] = useState('');
-  const [isImported, setIsImported] = useState(false);
+  const [isImported, setIsImported] = useState('');
+  const [goodsPrice, setGoodsPrice] = useState('');
   const [isAddGoodButtonDisabled, setIsAddGoodButtonDisabled] = useState(true);
+  
+  useEffect(() => {
+    if ((goodsName && goodsPrice && category && isImported) !== '' ) {
+      setIsAddGoodButtonDisabled(false);
+    }
+  }, [goodsName, goodsPrice, category, isImported]);
+
 
   return (
     <div className='form'>
       <h3 className='formHeader'>Add New Goods</h3>
       <div className='inputHolder'>
-        <input type='text' placeholder='Goods Name' className='nameInput'></input>
-        <select id='category-select' className='categorySelect'>
-          <option value='' disabled selected>Select Category</option>
-          <option value='food'>Food</option>
-          <option value='book'>Book</option>
-          <option value='medical'>Medical Product</option>
-          <option value='other'>Other</option>
-        </select>
-        <select id='imported-select' className='importedSelect'>
-          <option value='' disabled selected>Imported?</option>
-          <option value='no'>No</option>
-          <option value='yes'>Yes</option>
-        </select>
-        <input type='text' placeholder='Price' className='priceInput'></input>
-        <button disabled={isAddGoodButtonDisabled}>Add Good</button>
+        <div className='formOptions'>
+          <input 
+            type='text' 
+            placeholder='Goods Name' 
+            className='nameInput'
+            onChange={e => setGoodsName(e.target.value)}
+          />
+          <select 
+            id='category-select' 
+            className='categorySelect' 
+            value={category} 
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value='' disabled>Select Category</option>
+            <option value='food'>Food</option>
+            <option value='book'>Book</option>
+            <option value='medical'>Medical Product</option>
+            <option value='other'>Other</option>
+          </select>
+          <select 
+            id='imported-select' 
+            className='importedSelect'
+            value={isImported}
+            onChange={e => setIsImported(e.target.value)}
+          >
+            <option value='' disabled>Imported?</option>
+            <option value='no'>No</option>
+            <option value='yes'>Yes</option>
+          </select>
+          <input 
+            type='number'
+            placeholder='Price' 
+            className='priceInput'
+            onChange={e => setGoodsPrice(e.target.value)}
+          />
+        </div>
+        <button 
+          disabled={isAddGoodButtonDisabled} 
+          className={isAddGoodButtonDisabled ? 'buttonDisabled addGoodsButton' : 'buttonEnabled addGoodsButton'}
+        >
+          Add Good
+        </button>
       </div>
     </div>
   );
