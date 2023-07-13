18a102bf028e52b2b5c60e256606f58f402ae18f
Leah Winters <leahwinters@Leahs-MBP.home>
2023-07-13 17:12:29 -0400

    receiptsFeature - updated /CurrentGoods to handle checkout functionality and did small refactor

--- src/CurrentGoods/CurrentGoods.tsx
+++ src/CurrentGoods/CurrentGoods.tsx
@@ -1,13 +1,15 @@
 import { FC, useEffect, useState } from 'react';
-import AddedGoodsDataInterface from '../interfacesData';
+import { AddedGoodsDataInterface } from '../interfacesData';
 import './CurrentGoods.css';
 
 interface ICurrentGoods {
   currentGoods: Array<AddedGoodsDataInterface>;
   removeGoodFromCurrentGoods: (goodsData: AddedGoodsDataInterface) => void;
+  calculateAndAddTaxes: () => void;
+  clearCurrentGoods: () => void;
 }
 
-const CurrentGoods: FC<ICurrentGoods> = ({ currentGoods, removeGoodFromCurrentGoods }) => {
+const CurrentGoods: FC<ICurrentGoods> = ({ currentGoods, removeGoodFromCurrentGoods, calculateAndAddTaxes, clearCurrentGoods }) => {
   const [isCheckOutDisabled, setIsCheckOutDisabled] = useState(true);
 
   useEffect(() => {
@@ -18,24 +20,27 @@ const CurrentGoods: FC<ICurrentGoods> = ({ currentGoods, removeGoodFromCurrentGo
     }
   }, [currentGoods]);
 
+  const handleCheckOut = () => {
+    calculateAndAddTaxes();
+    clearCurrentGoods();
+  };
+
   const getCurrentGoodsToDisplay = () => {
     if (currentGoods.length > 0) {
-      return currentGoods.map(good => {
-        return (
-          <tr id={good.name}>
-            <td className='cell'>{good.name}</td>
-            <td className='cell'>{good.price}</td>
-            <td className='deleteBtnHolder'>
-              <button 
-                className='deleteBtn' 
-                onClick={() => removeGoodFromCurrentGoods(good)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-      });
+      return currentGoods.map(good => (
+        <tr id={good.name}>
+          <td className='cell'>{good.name}</td>
+          <td className='cell'>{good.price}</td>
+          <td className='deleteBtnHolder'>
+            <button
+              className='deleteBtn'
+              onClick={() => removeGoodFromCurrentGoods(good)}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      ));
     } else {
       return (
         <tr>
@@ -45,29 +50,32 @@ const CurrentGoods: FC<ICurrentGoods> = ({ currentGoods, removeGoodFromCurrentGo
     };
   };
 
-  const getTableToDisplay = () => {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th className='costCell'>Cost Before Tax</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {getCurrentGoodsToDisplay()}
-        </tbody>
-      </table>
-    );
-  };
+  const getTableToDisplay = () => (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th className='costCell'>Cost Before Tax</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {getCurrentGoodsToDisplay()}
+      </tbody>
+    </table>
+  );
 
   return (
     <div className='currentGoodsHolder'>
       <h3>Current Goods</h3>
       <div className='tableholder'>
         {getTableToDisplay()}
-        <button className={isCheckOutDisabled ? 'checkOutBtn buttonDisabled' : 'checkOutBtn buttonEnabled'}>Check Out</button>
+        <button 
+          className={isCheckOutDisabled ? 'checkOutBtn buttonDisabled' : 'checkOutBtn buttonEnabled'}
+          onClick={() => handleCheckOut()}
+          >
+            Check Out
+        </button>
       </div>
     </div>
   );
